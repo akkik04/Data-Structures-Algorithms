efaddba7e3f1ce0f3cc8827e24c2490b3adb2819
akkik04 <81925146+akkik04@users.noreply.github.com>
2021-05-17 22:34:22 -0400

    Migratory Birds Solution.

---
+++ Data Structures and Algorithms/HackerRank Algo Solutions/EASY PROBLEMS/MigratoryBirds.py
@@ -0,0 +1,22 @@
+# GRADING STUDENTS HACKER-RANK SOLUTION:
+
+# function receiving parameter of the array.
+def migratoryBird(arr):
+
+    # initializing a list of zeroes.
+    birds = [0] * len(arr)
+
+    # creating a for-loop to iterate for the array.
+    for i in range (len(arr)):
+
+        # incrementing the count for each type of bird.
+        birds[arr[i]] += 1
+    
+    # returning most common index.
+    return birds.index(max(birds))
+
+
+arr_count = int(input().strip())
+arr = list(map(int, input().rstrip().split()))
+
+print(migratoryBird(arr))
