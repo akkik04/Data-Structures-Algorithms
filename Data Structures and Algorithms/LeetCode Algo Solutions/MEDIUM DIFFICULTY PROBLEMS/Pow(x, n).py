59e355383e3d4dad7988b57c2e744a07580826e9
Akshith Kandivanam <81925146+akkik04@users.noreply.github.com>
2021-07-14 13:00:04 -0400

    Pow(x, n) Solution.

---
+++ Data Structures and Algorithms/LeetCode Algo Solutions/MEDIUM DIFFICULTY PROBLEMS/Pow(x, n).py
@@ -0,0 +1,10 @@
+# POW(X, N) LEETCODE SOLUTION:
+
+# creating a class.
+class Solution(object):
+
+    # creating a function to solve the problem.
+    def myPow(self, x, n):
+
+        # returning the value of 'x' raised to the power of 'n'
+        return pow(x, n)
