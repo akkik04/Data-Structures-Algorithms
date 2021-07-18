3e61f8ff3516140e5211669d583e7970fb0529c3
Akshith Kandivanam <81925146+akkik04@users.noreply.github.com>
2021-07-18 14:52:29 -0400

    Reverse String Solution.

---
+++ Data Structures and Algorithms/LeetCode Algo Solutions/EASY DIFFICULTY PROBLEMS/ReverseString.py
@@ -0,0 +1,15 @@
+# REVERSE STRING LEETCODE SOLUTION:
+
+# creating a class.
+class Solution(object):
+
+    # creating a function to solve the problem.
+    def reverseString(self, s):
+
+        '''
+        We are not returning anything as the problem specified, we simply modify the array.
+        
+        '''
+
+        # code to modify the array of strings into its reversed form.
+        s.reverse()
