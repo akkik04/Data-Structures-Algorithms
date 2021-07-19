c5a5f1b6d4b6290b47e42182bbb8e2e9354698f3
Akshith Kandivanam <81925146+akkik04@users.noreply.github.com>
2021-07-18 21:02:53 -0400

    To Lower Case Solution.

---
+++ Data Structures and Algorithms/LeetCode Algo Solutions/EASY DIFFICULTY PROBLEMS/ToLowerCase.py
@@ -0,0 +1,10 @@
+# TO LOWER CASE LEETCODE SOLUTION:
+
+# creating a class.
+class Solution(object):
+
+    # creating a function to solve the problem.
+    def toLowerCase(self, s):
+
+        # returning the lowercase version of the string.
+        return s.lower()
