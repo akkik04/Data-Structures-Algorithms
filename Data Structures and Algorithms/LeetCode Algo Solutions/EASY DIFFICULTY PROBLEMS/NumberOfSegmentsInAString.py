e1278be55a1b3f167bdfa6ca4b648b97ec295eea
Akshith Kandivanam <81925146+akkik04@users.noreply.github.com>
2021-07-21 14:55:17 -0400

    Number of Segments in a String Solution.

---
+++ Data Structures and Algorithms/LeetCode Algo Solutions/EASY DIFFICULTY PROBLEMS/NumberOfSegmentsInAString.py
@@ -0,0 +1,10 @@
+# NUMBER OF SEGMENTS IN A STRING LEETCODE SOLUTION:
+
+# creating a class.
+class Solution (object):
+
+    # creating a function to solve the problem.
+    def countSegments(self, s):
+
+        # returning the length of the string split.
+        return len(s.split())
