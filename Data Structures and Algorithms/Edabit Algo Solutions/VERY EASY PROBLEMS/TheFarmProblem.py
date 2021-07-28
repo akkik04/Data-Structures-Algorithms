8893a15b78907e881b975a63ffb9612a278b3011
Akshith Kandivanam <81925146+akkik04@users.noreply.github.com>
2021-07-28 17:22:39 -0400

    The Farm Problem Solution.

---
+++ Data Structures and Algorithms/Edabit Algo Solutions/VERY EASY PROBLEMS/TheFarmProblem.py
@@ -0,0 +1,7 @@
+# THE FARM PROBLEM EDABIT SOLUTION:
+
+# creating a function to solve the problem.
+def animals(chickens, cows, pigs):
+
+    # returning the sum of all legs for the animals.
+    return (2 * chickens) + (4 * cows) + (4 * pigs)
