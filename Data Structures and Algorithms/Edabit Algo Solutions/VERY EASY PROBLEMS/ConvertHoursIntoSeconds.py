d912dd9394b77c1ef163e61fe225c8b97a342e6b
Akshith Kandivanam <81925146+akkik04@users.noreply.github.com>
2021-08-01 21:33:05 -0400

    Convert Hours into Seconds Solution.

---
+++ Data Structures and Algorithms/Edabit Algo Solutions/VERY EASY PROBLEMS/ConvertHoursIntoSeconds.py
@@ -0,0 +1,7 @@
+# CONVERT HOURS INTO SECONDS EDABIT SOLUTION:
+
+# creating a function to solve the problem.
+def how_many_seconds(hours):
+  
+  # returning the hours in seconds.
+  return hours * 3600
