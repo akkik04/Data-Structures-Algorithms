26e36028ece9f3e006410fa6a6c4a250b942e80d
Akshith Kandivanam <81925146+akkik04@users.noreply.github.com>
2021-08-01 22:34:36 -0400

    Find the Missing Number Solution.

---
+++ Data Structures and Algorithms/Edabit Algo Solutions/EASY PROBLEMS/FindTheMissingNumber.py
@@ -0,0 +1,7 @@
+# FIND THE MISSING NUMBER EDABIT SOLUTION:
+
+# creating a function to solve the problem.
+def missing_num(lst):
+  
+  # returning the sum of the given list substracted from the intial sum of the numbers from 1-10.
+  return 55 - sum(lst)
