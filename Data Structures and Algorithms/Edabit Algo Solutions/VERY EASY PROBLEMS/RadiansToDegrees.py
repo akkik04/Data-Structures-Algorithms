bb7adbe79f4b95f79ab68967814486e5893b2d21
Akshith Kandivanam <81925146+akkik04@users.noreply.github.com>
2021-08-03 16:17:20 -0400

    Radians to Degrees Solution.

---
+++ Data Structures and Algorithms/Edabit Algo Solutions/VERY EASY PROBLEMS/RadiansToDegrees.py
@@ -0,0 +1,12 @@
+# RADIANS TO DEGREES EDABIT SOLUTION:
+
+# importing the neccessary math module.
+import math
+
+# creating a function to solve the problem.
+def radians_to_degrees(rad):
+  
+  # returning the radians in degrees.
+  return round(math.degrees(rad), 1)
+  
+  
