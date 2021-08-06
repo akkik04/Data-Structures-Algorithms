4b9768291ac4ec1a3cef99ee913f6c685d233abc
Akshith Kandivanam <81925146+akkik04@users.noreply.github.com>
2021-08-06 14:35:18 -0400

    Purge and Organize Solution.

---
+++ Data Structures and Algorithms/Edabit DS Solutions/EASY PROBLEMS/PurgeAndOrganize.py
@@ -0,0 +1,7 @@
+# PURGE AND ORGANIZE EDABIT SOLUTION:
+
+# creating a function to solve the problem.
+def unique_sort(lst):
+  
+  # returning the sorted list without duplicates.
+  return sorted(set(lst))
