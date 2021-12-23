a15e1f57cae1a3310ea4585588a9621b451b10eb
Akshith Kandivanam <81925146+akkik04@users.noreply.github.com>
2021-12-23 18:41:28 -0500

    del directory

--- Data Structures and Algorithms/CodingBatExercises/Warmup-1/String-1/HelloName.py
+++
@@ -1,4 +0,0 @@
-def hello_name(name):
-  
-  # returning a greeting to the name entered.
-  return "Hello" + name + "!"
--- Data Structures and Algorithms/CodingBatExercises/Warmup-2/StringTimes.py
+++
@@ -1,6 +0,0 @@
-# STRING TIMES CODING BAT SOLUTION:
-
-def string_times(str, n):
-  
-  # returning the string 'n' times.
-  return str * n
