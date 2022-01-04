b425c529e2f502f4702e84f2c0d17973c668e727
Akshith Kandivanam <81925146+akkik04@users.noreply.github.com>
2022-01-04 16:21:22 -0500

    optimized sol (1 ms runtime).
    Optimized solution for the 'Find Numbers with Even Number of Digits' Leetcode problem.

--- Data Structures and Algorithms/LeetCode Algo Solutions/EASY DIFFICULTY PROBLEMS/FindNumbersWithEvenNumberOfDigits.java
+++ Data Structures and Algorithms/LeetCode Algo Solutions/EASY DIFFICULTY PROBLEMS/FindNumbersWithEvenNumberOfDigits.java
@@ -3,27 +3,44 @@
 // importing the scanner object.
 import java.util.Scanner;
 
-public class FindNumbersWithEvenNumberOfDigits {
+public class FindNumbersWithEvenNumberOfDigits{
 
-    // creating a function to find the count of even digits in a given array.
-    public static int evenDigits(int arr[]) {
+    // creating a function to count the number of elements with even number of digits.
+    public static int countEvenDigitElements(int arr[]) {
 
-        // creating a variable to track the count of even digit elements.
-        int count = 0;
+        // creating a variable to track the total digits in an element. 
+        int digitCount = 0;
+
+        // creating a variable to track the number of elements with even number of digits.
+        int counter = 0;
 
         // creating a for-loop to iterate for the length of the array.
-        for (int j=0; j < arr.length; j++){
+        for (int i=0; i < arr.length; i++){
 
-            // creating an if-statement to check for elements that have even number of digits.
-            if (String.valueOf(arr[j]).length() % 2 == 0){
+            // creating a variable to tie the current element.
+            int curr_el = arr[i];
 
-                // code to increment the count if the condition is satisfied.
-                count++;
+            // creating a while-loop to find the total number of digits in the element.
+            while (curr_el != 0){
+
+                // incrementing the 'digitCount' after each iteration.
+                curr_el /= 10;
+                digitCount++;
             }
+
+            // creating an if-statement to check if the element has an even number of digits.
+            if (digitCount % 2 == 0){
+
+                // code to increment the 'counter' if the condition is met.
+                counter++;
+            }
+
+            // code to set the 'digitCount' back to '0' for processing the next element in the array.
+            digitCount = 0;
         }
-        
-        // returning the value of the count.
-        return count;
+
+        // returning the counter storing the number of elements with even digits.
+        return counter;
     }
 
     // main method.
@@ -32,21 +49,21 @@ public class FindNumbersWithEvenNumberOfDigits {
         // instantiating the scanner object.
         Scanner kp = new Scanner(System.in);
 
-        // creating a variable to take input for the length of the array.
+        // creating a variable to take input for the array size.
         int n = kp.nextInt();
 
         // creating an array of desired length.
         int arr[] = new int[n];
 
-        // creating a for-loop to iterate for the length of the array to take input for each index.
+        // creating a for-loop to iterate for the length of the array, taking input for each index.
         for (int i=0; i < arr.length; i++){
 
-            // code to take input for each index of the array.
+            // code to receive input for each index.
             arr[i] = kp.nextInt();
         }
 
         // creating a variable to store and print the value returned by the function.
-        int answer = evenDigits(arr);
+        int answer = countEvenDigitElements(arr);
         System.out.println(answer);
     }
 }
